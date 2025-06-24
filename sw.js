0458912bc71b2d509b5981cf7a4ba5e1070b21f9
Thanh <32680850+thanhsr@users.noreply.github.com>
2025-06-24 15:15:52 +0700

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,6 @@
+self.addEventListener('notificationclick', function(event) {
+    event.notification.close();
+    event.waitUntil(
+        clients.openWindow(event.notification.data.url)
+    );
+});
